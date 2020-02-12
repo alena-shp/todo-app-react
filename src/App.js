2290c5786292b85540f7bdca07db12bb5120307d
Shpakova Alena <shpakovadev@gmail.com>
2020-02-12 13:06:07 +0300

    create state todos

--- src/App.js
+++ src/App.js
@@ -5,13 +5,33 @@ import SearchTodo from "./SearchTodo"
 import TodoList from "./TodoList"
 
 class App extends React.Component {
+  idStart = 1
+
+  state = {
+    todoData: [
+      this.CreateItem("buy coffee"),
+      this.CreateItem("order a book"),
+      this.CreateItem("to water flowers")
+    ]
+  }
+  CreateItem(label) {
+    return {
+      label,
+      important: false,
+      done: false,
+      id: this.idStart++
+    }
+  }
+
   render() {
+    const { todoData } = this.state
     return (
       <div className="app">
+        <h1 className="app__header">Todo List</h1>
         <CounterTodo />
         <FilterTodo />
         <SearchTodo />
-        <TodoList />
+        <TodoList todos={todoData}/>
       </div>
     )
   }
